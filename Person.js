a8cbf6286b7c362aaadbb58e97cd5f79dbf1fd16
Emma King <emmailonaking@gmail.com>
2022-06-20 11:38:12 +0300

    My suggestions for person class, age, pet

--- Person.js
+++ Person.js
@@ -1,8 +1,12 @@
 class Person {
-  constructor(name) {
+  constructor(name, age, pet) {
     this.name = name;
+    this.age = age;
+    this.pet = pet;
   }
   greet() {
     console.log(`Hello, my name is ${this.name}`);
+    console.log(`I am ${this.age} years old`);
+    console.log(`My pet is ${this.pet}`);
   }
 }
